ecce99b436a196b20a76bb82da3730db614e2441
Jhossuan <jhossuacamposd86@gmail.com>
2026-05-08 02:15:04 -0500

    chore: env validator added

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,12 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { envValidation } from './config/env.validation'
+import {Logger} from "@nestjs/common";
 
 async function bootstrap() {
+  const logger = new Logger("Main");
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  await app.listen(envValidation.port);
+  logger.log(`Notevo API is running on http://localhost:${envValidation.port}`);
 }
 bootstrap();
