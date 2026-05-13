522a28e10fefde1ef2b96f2430f4456100e52a65
Jhossuan <jhossuacamposd86@gmail.com>
2026-05-13 01:01:00 -0500

    chore: add docker-compose with postgres, redis and rabbitmq, and some validations in env.validation

--- src/config/env.validation.ts
+++ src/config/env.validation.ts
@@ -3,10 +3,14 @@ import * as joi from 'joi'
 
 interface EnvVars {
     PORT: number;
+    NODE_ENV: string;
+    DATABASE_URL: string;
 }
 
 const envsSchema = joi.object({
-    PORT: joi.number().required(),
+    PORT: joi.number().default(3000),
+    NODE_ENV: joi.string().valid('production', 'development', 'test').required(),
+    DATABASE_URL: joi.string().required(),
 }).unknown(true)
 
 const { error, value } = envsSchema.validate(process.env);
@@ -19,4 +23,6 @@ const envVars: EnvVars = value;
 
 export const envValidation = {
     port: envVars.PORT,
+    nodeEnv: envVars.NODE_ENV,
+    databaseUrl: envVars.DATABASE_URL,
 }
